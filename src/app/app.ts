4bd3a1e045fc7d1e1756cb0c287478d31488af38
Fatma Abdelaal <fatma.ali@datagearbi.com>
2025-07-25 11:53:56 +0300

    - first commit for the assignment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './components/navbar/navbar';
+import {Footer} from './components/footer/footer';
+import {Shredsection} from './components/shared/shredsection/shredsection';
+import {Home} from './components/home/home';
+import {Portfolio} from './components/portfolio/portfolio';
+import {About} from './components/about/about';
+import {Contact} from './components/contact/contact';
+import {SharedInfo} from './components/shared/shared-info/shared-info';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Footer, RouterOutlet, SharedInfo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Session2Week3');
+  protected readonly title = signal('Assignment1');
 }
